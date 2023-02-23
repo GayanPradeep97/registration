f73b13fc50eaacbb3b9824087d65686b11236d5f
GayanPradeep97 <gayanpradeep0212@gmail.com>
2023-02-23 05:38:22 -0800

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import './app.css'
 
 const App = () => {
   return (
-    <h2>App</h2>
+    <div>
+        <h1>Register</h1>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="email" name="email" />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+          <br />
+          <label>
+            Confirm Password:
+            <input type="password" name="confirmPassword" />
+          </label>
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      </div>
   )
 }
 
